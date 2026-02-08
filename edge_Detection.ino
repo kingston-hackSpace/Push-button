b7b5a2048ea80d3914a1fcd1de0ea467bc3261f7
CamilaColussi-KSA <c.colussi@kingston.ac.uk>
2026-02-08 17:45:04 +0000

    Create edge_Detection.ino

---
+++ edge_Detection.ino
@@ -0,0 +1,29 @@
+// EDGE DETECTION
+
+const int buttonPin = 2;
+const int ledPin = 7;
+
+int buttonState = 0;
+int lastButtonState = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+
+  // detect change
+  if (buttonState != lastButtonState) {
+    if (buttonState == LOW) {
+      digitalWrite(ledPin, HIGH);
+      Serial.println("pressed")
+    }else{
+      digitalWrite(ledPin, LOW);
+      Serial.println("released");
+    }
+  }
+  lastButtonState = buttonState;
+}
